1cbaeb49d3927332a0f5676f5796e457f2a5d418
Gergő Papp <imgergopapp@gmail.com>
2019-03-12 13:04:49 +0100

    Methods moved to ServletHelper

--- src/main/java/com/codecool/web/servlet/RegServlet.java
+++ src/main/java/com/codecool/web/servlet/RegServlet.java
@@ -1,8 +1,7 @@
 package com.codecool.web.servlet;
 
-import com.codecool.web.model.Greeting;
 import com.codecool.web.model.User;
-import com.codecool.web.service.GreetingService;
+import com.codecool.web.service.ServletHelper;
 import com.codecool.web.service.XMLparser;
 
 import javax.servlet.ServletException;
@@ -12,10 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.util.List;
 
 @WebServlet("/reg")
 public class RegServlet extends HttpServlet {
@@ -34,14 +29,17 @@ public class RegServlet extends HttpServlet {
         String user_pass = request.getParameter("psw");
 
         Boolean position = false;
-        if (request.getParameter("position").equals("mentor")){
+        if (request.getParameter("position").equals("mentor")) {
             position = true;
         }
 
-
-        User u = new User(user_name, user_email, user_pass,position );
-        XMLparser.write(u, asd);
-
-        response.sendRedirect("login.html");
+        if(ServletHelper.isValidEmail(user_email, asd)){
+            User u = new User(user_name, user_email, user_pass, position);
+            XMLparser.write(u, asd);
+            response.sendRedirect("login.html");
+        }
+        else {
+            response.sendRedirect("registration.html");
+        }
     }
 }
