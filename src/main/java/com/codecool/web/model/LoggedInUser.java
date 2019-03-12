d801b4fca4c1afbed28ef8761dead1643b8e7ef3
Gergő Papp <imgergopapp@gmail.com>
2019-03-12 14:19:21 +0100

    Added (temp) class

---
+++ src/main/java/com/codecool/web/model/LoggedInUser.java
@@ -0,0 +1,13 @@
+package com.codecool.web.model;
+
+public class LoggedInUser {
+    private static String emailAddress;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
