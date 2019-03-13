4fb768755d8767f7517cac1cb9fba65278c23349
Gergő Papp <imgergopapp@gmail.com>
2019-03-13 16:19:23 +0100

    Added new method & field for name

--- src/main/java/com/codecool/web/model/LoggedInUser.java
+++ src/main/java/com/codecool/web/model/LoggedInUser.java
@@ -3,7 +3,17 @@ package com.codecool.web.model;
 public class LoggedInUser{
     private static String emailAddress;
 
-    public String getEmailAddress() {
+    public static String getLoggedInUserName() {
+        return LoggedInUserName;
+    }
+
+    public static void setLoggedInUserName(String loggedInUserName) {
+        LoggedInUserName = loggedInUserName;
+    }
+
+    private static String LoggedInUserName;
+
+    public static String getEmailAddress() {
         return emailAddress;
     }
 
