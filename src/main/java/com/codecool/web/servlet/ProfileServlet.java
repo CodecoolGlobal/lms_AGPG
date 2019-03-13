7e93ec8113f2f041e5f5dc0445ad68f90ce9b1b5
Taraszovics Péter <gtarapeti@gmail.com>
2019-03-13 13:11:16 +0100

    created profile servlet for listing users

--- src/main/java/com/codecool/web/servlet/ProfileServlet.java
+++ src/main/java/com/codecool/web/servlet/ProfileServlet.java
@@ -1,4 +1,25 @@
 package com.codecool.web.servlet;
 
-public class ProfileServlet {
+import com.codecool.web.model.User;
+import com.codecool.web.model.Users;
+import com.codecool.web.service.XMLparser;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/profile")
+public class ProfileServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String asd = request.getServletContext().getRealPath("data.xml");
+
+    }
+
 }
+
