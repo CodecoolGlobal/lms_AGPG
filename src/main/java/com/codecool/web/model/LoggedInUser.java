b0713cb0bd41d7f44f2aea8c9e002ff8b12b8dbd
Taraszovics Péter <gtarapeti@gmail.com>
2019-03-14 10:09:32 +0100

    created oldpw in loggedinuser class

--- src/main/java/com/codecool/web/model/LoggedInUser.java
+++ src/main/java/com/codecool/web/model/LoggedInUser.java
@@ -2,9 +2,18 @@ package com.codecool.web.model;
 
 public class LoggedInUser{
     private static String emailAddress;
+    private static String oldPassw;
 
     public static String getLoggedInUserName() {
         return LoggedInUserName;
+    }
+
+    public static String getOldPassw() {
+        return oldPassw;
+    }
+
+    public static void setOldPassw(String oldPassw) {
+        LoggedInUser.oldPassw = oldPassw;
     }
 
     public static void setLoggedInUserName(String loggedInUserName) {
