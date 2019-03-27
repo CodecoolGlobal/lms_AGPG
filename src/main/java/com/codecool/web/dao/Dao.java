161d55174440b5083071f8d05dc05020396ba19b
Gergő Papp <imgergopapp@gmail.com>
2019-03-27 09:10:47 +0100

    Changed sql script

--- src/main/java/com/codecool/web/dao/Dao.java
+++ src/main/java/com/codecool/web/dao/Dao.java
@@ -9,13 +9,17 @@ import java.util.List;
 
 public class Dao {
     public static String getDao(Connection connection) throws SQLException {
-        String sql = "SELECT * FROM cities";
+        //String sql = "SELECT * FROM cities";
+        String sql = "INSERT INTO cities (name)" +
+            "VALUES ('TEST')";
         String s = "";
         try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
-            while (resultSet.next()) {
+             ) {
+            statement.executeQuery(sql);
+             //ResultSet resultSet = statement.executeQuery(sql)) {
+           /* while (resultSet.next()) {
                 s = resultSet.getString("name");
-            }
+            }*/
         }
         return s;
     }
