d824f61ec42030aaca7193afbf360301598f09c6
Gergő Papp <imgergopapp@gmail.com>
2019-04-09 18:30:01 +0200

    Implemented isDateUsed, setUsersPresent methods

--- src/main/java/com/codecool/web/util/AttendanceUntil.java
+++ src/main/java/com/codecool/web/util/AttendanceUntil.java
@@ -1,7 +1,10 @@
 package com.codecool.web.util;
 
 
+import com.codecool.web.model.User;
+
 import java.sql.*;
+import java.util.List;
 
 
 public class AttendanceUntil {
@@ -19,6 +22,30 @@ public class AttendanceUntil {
             resultSet.next();
             return resultSet.getBoolean(3);
         }
+    }
+
+    public static boolean isDateUsed(Connection connection, Date sqlDate) throws SQLException {
+        String sql = "SELECT DISTINCT ON(date_att) * FROM attendance WHERE date_att = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            statement.setDate(1, sqlDate);
+            ResultSet resultSet = statement.executeQuery();
+            return resultSet.next();
+        }
+    }
+
+    public static List<User> setUsersPresent(Connection connection, Date sqlDate, List<User> users) throws SQLException {
+        for (User user : users) {
+            String sql = "SELECT * FROM attendance WHERE date_att = ? AND user_id = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)
+            ) {
+                statement.setDate(1, sqlDate);
+                statement.setInt(2, user.getId());
+                ResultSet resultSet = statement.executeQuery();
+                user.setPresent(resultSet.next());
+            }
+        }
+        return users;
     }
 
 
