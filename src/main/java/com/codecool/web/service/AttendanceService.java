84374f909aba1815ea093dde40a83775a5d45b1c
Gergő Papp <imgergopapp@gmail.com>
2019-04-09 18:21:14 +0200

    Added method: getToday, getTomorrow, getYesterday

--- src/main/java/com/codecool/web/service/AttendanceService.java
+++ src/main/java/com/codecool/web/service/AttendanceService.java
@@ -1,12 +1,14 @@
 package com.codecool.web.service;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class AttendanceService {
-    public static long getMilis(String date){
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    public static long getMilis(String date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
         try {
             Date convertedCurrentDate = sdf.parse(date);
             long time = convertedCurrentDate.getTime();
@@ -17,4 +19,36 @@ public class AttendanceService {
         return 0;
     }
 
+    public static String getCurrentDate() { // Used in attendance.jsp
+        String pattern = "MM/dd/yyyy";
+        DateFormat df = new SimpleDateFormat(pattern);
+        Date today = Calendar.getInstance().getTime();
+        return df.format(today);
+    }
+
+    public static String getYesterday() {
+        String pattern = "MM/dd/yyyy";
+        DateFormat df = new SimpleDateFormat(pattern);
+        Date date = new Date();
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.DATE, -1);
+
+        date = c.getTime();
+        return df.format(date);
+    }
+
+    public static String getTomorrow() {
+        String pattern = "MM/dd/yyyy";
+        DateFormat df = new SimpleDateFormat(pattern);
+        Date date = new Date();
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.DATE, 1);
+
+        date = c.getTime();
+        return df.format(date);
+    }
 }
